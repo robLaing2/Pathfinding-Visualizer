75870a0ab5fc0e8b277e29d8dad13263c4086ab3
robLaing2 <robert.laing.15@gmail.com>
2020-05-08 02:29:59 +0100

    React app setup

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import PathfindingVisualiser from './Pathfinder/PathfindingVisualiser'
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualiser></PathfindingVisualiser>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Pathfinder/PathfindingVisualiser.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Node from './Node';
+
+import './PathfindingVisualiser.css'
+
+export default class PathfindingVisualiser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Foo
+            </div>
+        );
+    }
+}
